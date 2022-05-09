775623aeee0ff86bec8719d4a42a87e7129d86a5
adenhercules <adenhercules07@gmail.com>
2022-05-09 01:39:21 -0400

    projeto quase finalizado, faltando ajustes no .js

---
+++ script.js
@@ -0,0 +1,19 @@
+var email=document.getElementById('email');
+var password=document.getElementById('password');
+
+email.addEventListener('focus',()=>{
+    email.style.borderColor="#ccc";
+});
+
+email.addEventListener('blur',()=>{
+    email.style.borderColor="#ccc";
+});
+
+password.addEventListener('focus',()=>{
+    password.style.borderColor="#ccc";
+});
+password.addEventListener('blur',()=>{
+    password.style.borderColor="#ccc";
+});
+
+
